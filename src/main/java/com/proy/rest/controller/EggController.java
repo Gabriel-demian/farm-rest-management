a1e8c4f1d2d4d2351a3bd7c3006918d9ff22d4f0
Gabriel <57782295+Gabriel-demian@users.noreply.github.com>
2020-12-04 00:29:02 -0300

    Se agrega @CrossOrigin en los controllers
    @CrossOrigin para que nos permite el acceso a los recursos desde otras ubicaciones.

--- src/main/java/com/proy/rest/controller/EggController.java
+++ src/main/java/com/proy/rest/controller/EggController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import com.proy.rest.services.FarmService;
 
 @RestController
 @RequestMapping("/api/eggs")
+@CrossOrigin("http://localhost:3000")
 public class EggController {
 	
 	@Autowired
